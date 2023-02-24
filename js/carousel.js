6f2f824f0323d807377817a3443677c2ef947c01
Serhii Bodnar <serhiivbodnar@gmail.com>
2023-02-25 01:27:15 +0200

    photo fixed

--- js/carousel.js
+++ js/carousel.js
@@ -3,15 +3,12 @@
 
     const slides = [
         '<div><img src="img/f150mods.jpg" alt="Ford"></div>',
-        '<div><img src="img/f150-baja.webp" alt="Ford"></div>',
-        '<div><img src="img/f150-mods-camp.webp" alt="Ford"></div>',
         '<div><img src="img/f150-raptor-mods.jpg" alt="Ford"></div>',
         '<div><img src="img/f150-raptor.jpg" alt="Ford"></div>',
         '<div><img src="img/f150camp.jpg" alt="Ford"></div>',
         '<div><img src="img/f150galery.jpg" alt="Ford"></div>',
         '<div><img src="img/ram-2500.jpg" alt="Ram"></div>',
         '<div><img src="img/ram-camp.jpg" alt="Ram"></div>',
-        '<div><img src="img/ram-galery.webp" alt="Ram"></div>',
         '<div><img src="img/ram-mods.jpg" alt="Ram"></div>',
         '<div><img src="img/tundra-mods.jpg" alt="Tundra"></div>',
         '<div><img src="img/tundra-galery.jpg" alt="Tundra"></div>',
